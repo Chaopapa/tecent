55bb3b8b0617b4c04fb3410a0de702ca50211c99
cc <1321607222@qq.com>
2019-10-11 15:23:50 +0800

    完成消息列表基本样式

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,19 @@
-
 // gulp模块
-const gulp = require('gulp');
+const { src } = require("gulp");
 //gulp服务模块
-const webserver = require('gulp-webserver');
-//中间件模块
-const mockDataMiddleware = require('../data/mockDataMiddleware');
+const webserver = require("gulp-webserver");
 
-gulp.task('webserver', ()=>{
-
-  gulp.src('./')//该文件夹是服务器的根路径
-  .pipe(
-    webserver({
-      host: '127.0.0.1',
-      port: '8082',
-      livereload: true,//热更新
-      directoryListing: true,//是否文件夹列表
-      open: true,//打开浏览器
-      // 中间件,拦截请求
-      middleware: mockDataMiddleware
-    })
-  );
-  
-});
+const server = function() {
+  return src("./") //该文件夹是服务器的根路径
+    .pipe(
+      webserver({
+        host: "127.0.0.1",
+        port: "8082",
+        livereload: true, //热更新
+        directoryListing: true, //是否文件夹列表
+        open: true, //打开浏览器
+        // 中间件,拦截请求
+        middleware: require("../data/mockDataMiddleware")
+      })
+    );
+};
---
+++ src/views/message/main/main.js
@@ -0,0 +1,5 @@
+;(function(){
+    var messageList = new MyScroll(
+        '.message-main'
+    );
+})()
